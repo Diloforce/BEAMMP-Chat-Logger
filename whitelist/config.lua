ebbb21a2fe2079b8777853f2705bd78d3ccb55d3
Diloforce <150070160+Diloforce@users.noreply.github.com>
2024-02-29 20:40:11 +0000

    REVAMP
    Whitelist delete

--- whitelist/config.lua
+++
@@ -1,8 +0,0 @@
--- Configuration for Whitelist Mode
-local config = {
-    mode = "local" -- Change to "cmd"  to use the command-based whitelist or "local" for adding names to a list
-}
-
-function getConfig()
-    return config
-end
