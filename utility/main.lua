8ab55595131c562aa46f027726a52cd141527b9a
Diloforce <150070160+Diloforce@users.noreply.github.com>
2024-03-01 19:59:59 +0000

    Update main.lua
    MAJOR UPDATE : AUTO BAN FOR  EXTREME BAD WORDS

--- utility/main.lua
+++ utility/main.lua
@@ -1,11 +1,33 @@
+local mytimer = MP.CreateTimer()
 print("Loading Plugins...")
+-- Requirements
+
+-- webhook
+
 
 -- Configurations
 local config = {
     mode = "local",  -- "local" for a static list, "cmd" for command-based management
     whitelistEnabled = false,  -- Enable or disable whitelist feature
-    censorEnabled = true  -- Enable or disable censor feature
+    censorEnabled = true,  -- Enable or disable censor feature
 }
+
+-- Integrations 
+util = {}
+
+util.readBanFile = function(rwSw, wrInput)
+    local path = "../blacklist"
+    local blFile = io.open(path, rwSw and "a+" or "r")
+    if rwSw and wrInput and blFile then
+        blFile:write(wrInput)
+        blFile:flush()
+    elseif not rwSw and blFile then
+        local content = blFile:read("*all")
+        blFile:close()
+        return content
+    end
+    if blFile then blFile:close() end
+end
 
 -- Censor
 function normalizeMessage(message)
@@ -15,7 +37,7 @@ function normalizeMessage(message)
 end
 
 function containsBadWord(normalizedMessage)
-    local badWords = {"nigger", "faggot", "slut", "retard", "whore", "cunt", "dickhead", "asshole", "motherfucker", "kys", "kill yourself", "im going to kill you", "nigga", "niga", "niger"}
+    local badWords = {"nigger", "faggot", "slut", "retard", "whore", "cunt", "asshole", "motherfucker", "kys", "kill yourself", "im going to kill you", "nigga", "niga"}
     for _, word in ipairs(badWords) do
         if normalizedMessage:find(word) then
             return true
@@ -25,13 +47,19 @@ function containsBadWord(normalizedMessage)
 end
 
 function MyChatMessageHandler(sender_id, sender_name, message)
-    if config.censorEnabled and containsBadWord(normalizeMessage(message)) then
-        MP.SendChatMessage(-1, .. sender_name ..  " Your message was censored. Do not repeat.")
-
-        print(sender_name .. "'s message was censored.")
-        return 1 
-    end
-    return 0  
+    local normalizedMessage = normalizeMessage(message)  
+    if normalizedMessage:find("nigger") or normalizedMessage:find("niga") or normalizedMessage:find("faggot") or normalizedMessage:find("niger") or normalizedMessage:find("nigga") then
+        MP.DropPlayer(sender_id, "You have been banned due to offensive language. Reason of Ban: " .. normalizedMessage) 
+        local i = "\nCensorship-banned: " .. sender_name .. " | Reason: " .. normalizedMessage
+        util.readBanFile(true, i) 
+        MP.SendChatMessage(-1, sender_name .. ": Your message was censored.")
+        print(sender_name .. "'s message was censored. Bad word used: " .. normalizedMessage)
+    else
+        MP.SendChatMessage(-1, sender_name .. ": Your message was censored.")
+        print(sender_name .. "'s message was censored. Bad word used: " .. normalizedMessage) 
+            return 1
+         end
+    return false 
 end
 
 -- Whitelist 
@@ -57,3 +85,4 @@ MP.RegisterEvent("onPlayerConnecting", "onPlayerConnecting")
 MP.RegisterEvent("onChatMessage", "MyChatMessageHandler")
 
 print("Plugins Loaded Successfully.")
+print("Time taken:"  ..  mytimer:GetCurrent()) -- print how much time elapsed
